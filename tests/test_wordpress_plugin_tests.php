a51482e2d19f0e70d6f97c49d9169d6bcf3c72f2
Benjamin J. Balter <ben@balter.com>
2012-07-02 10:33:40 -0400

    test wordpress version

--- tests/test_wordpress_plugin_tests.php
+++ tests/test_wordpress_plugin_tests.php
@@ -1,10 +1,28 @@
 <?php
-
+/**
+ * A simple test to test that that testing framwork is testing tests. Meta, huh?
+ * @package wordpress-plugins-tests
+ */
 class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
 	
-	function test_tests() {
+	/**
+	 * Run a simple test to ensure that the tests are running
+	 */
+	 function test_tests() {
+		 
+		 $this->assertTrue( true );
+		 
+	 }
+	
+	/**
+	 * Verify that WordPress is installed and is the version that we requested
+	 */
+	function test_wp_version() {
 		
-		$this->assertTrue( true );
+		if ( !get_env( 'TRAVIS_PHP_VERSION' ) )
+			$this->markTestSkipped( 'Not running on Travis CI' );
+			
+		$this->assertEquals( get_bloginfo( 'version' ), getenv( 'WP_VERSION' ) );
 		
 	}
 	

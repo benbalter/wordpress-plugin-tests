e912d73901574376e6808e2e3c14b71bc83b2f3c
scribu <mail@scribu.net>
2012-09-15 04:50:57 +0300

    don't separate tests dir from plugin dir

--- setup.sh
+++ setup.sh
@@ -11,24 +11,21 @@ export WP_TESTS_DIR=/tmp/wordpress-tests
 # Init database
 mysql -e 'CREATE DATABASE wordpress_test;' -uroot
 
-# Normalize plugin folder name
-plugin_slug=$( echo "${PWD##*/}" | tr -s  '[:upper:]'  '[:lower:]' )
-mkdir ../plugin
-mv * ../plugin
-cd ..
-
 # Grab specified version of WordPress from github
-wget -O wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
+wget -O /tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
 mkdir -p $WP_CORE_DIR
-tar --strip-components=1 -zxmf wordpress.tar.gz -C $WP_CORE_DIR
+tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
-# Set up testing framework
+# Grab testing framework and config file
 svn co --ignore-externals http://unit-tests.svn.wordpress.org/trunk/ $WP_TESTS_DIR
 
 wget -O $WP_TESTS_DIR/wp-tests-config.php https://raw.github.com/scribu/wordpress-plugin-tests/setup/wp-tests-config.php
 
 # Put various components in proper folders
-mv -f plugin/tests ./tests
-mv -f plugin $WP_CORE_DIR/wp-content/plugins/$plugin_slug
+plugin_slug=$(basename $(pwd))
+plugin_dir=$WP_CORE_DIR/wp-content/plugins/$plugin_slug
 
-cd tests
+cd ..
+mv $plugin_slug $plugin_dir
+
+cd $plugin_dir/tests

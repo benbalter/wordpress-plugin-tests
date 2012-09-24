f48c3e85886be094cfdb46e46522427e5bd716bc
Bryan Petty <bryan@ibaku.net>
2012-09-24 16:40:02 -0600

    Updated default tests config settings with those used for core tests right now (to fix multisite tests on Travis CI).

--- wp-tests-config.php
+++ wp-tests-config.php
@@ -1,7 +1,25 @@
 <?php
-/* Path to the WordPress codebase you'd like to test. Add a backslash in the end. */
 
+/* Path to the WordPress codebase you'd like to test. Add a backslash in the end. */
 define( 'ABSPATH', getenv( 'WP_CORE_DIR' ) . '/' );
+
+// Test with multisite enabled
+define( 'WP_TESTS_MULTISITE', (bool) getenv( 'WP_MULTISITE' ) );
+
+// Force known bugs
+// define( 'WP_TESTS_FORCE_KNOWN_BUGS', true );
+
+// Test with WordPress debug mode on
+define( 'WP_DEBUG', true );
+
+// ** MySQL settings ** //
+
+// This configuration file will be used by the copy of WordPress being tested.
+// wordpress/wp-config.php will be ignored.
+
+// WARNING WARNING WARNING!
+// These tests will DROP ALL TABLES in the database with the prefix named below.
+// DO NOT use a production database or one that is shared with something else.
 
 define( 'DB_NAME', 'wordpress_test' );
 define( 'DB_USER', 'root' );
@@ -10,33 +28,11 @@ define( 'DB_HOST', 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-define( 'WPLANG', '' );
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_DISPLAY', true );
-
 define( 'WP_TESTS_DOMAIN', 'example.org' );
 define( 'WP_TESTS_EMAIL', 'admin@example.org' );
 define( 'WP_TESTS_TITLE', 'Test Blog' );
-define( 'WP_TESTS_NETWORK_TITLE', 'Test Network' );
-define( 'WP_TESTS_SUBDOMAIN_INSTALL', true );
-$base = '/';
-
-define( 'WP_ALLOW_MULTISITE', (bool) getenv( 'WP_MULTISITE' ) );
-
-if ( WP_ALLOW_MULTISITE ) {
-	define( 'WP_TESTS_BLOGS', 'first,second,third,fourth' );
-}
-
-if ( WP_ALLOW_MULTISITE && !defined('WP_INSTALLING') ) {
-	define( 'SUBDOMAIN_INSTALL', WP_TESTS_SUBDOMAIN_INSTALL );
-	define( 'MULTISITE', true );
-	define( 'DOMAIN_CURRENT_SITE', WP_TESTS_DOMAIN );
-	define( 'PATH_CURRENT_SITE', '/' );
-	define( 'SITE_ID_CURRENT_SITE', 1);
-	define( 'BLOG_ID_CURRENT_SITE', 1);
-	//define( 'SUNRISE', TRUE );
-}
-
-$table_prefix  = 'wp_';
 
 define( 'WP_PHP_BINARY', 'php' );
+
+define( 'WPLANG', '' );
+$table_prefix  = 'wptests_';
